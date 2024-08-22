a5b3fd4b9dc10668499519799cf891d0ca747d3d
Jan Carlo Cardama <jcmcardama@gmail.com>
2024-08-22 22:45:14 +0800

    fix height

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Grid, styled } from '@mui/material';
+
+const StyledGrid = styled(Grid)(() => {
+    return {
+      height: 'calc(100% - 128px)'
+    };
+});
 
 function App() {
   return (
-    <>
+    <Grid sx={{ height: "100vh" }}>
       <Header />
-      <Outlet />
+      <StyledGrid item>
+        <Outlet />
+      </StyledGrid>
       <Footer />
-    </>
+    </Grid>
   )
 }
 
